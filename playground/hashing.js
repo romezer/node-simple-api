6e4a4130db78c58698a73bf42a563d90d4f2e46e
Rom Ezer <rom@dotbcs.com>
2018-11-25 15:01:18 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,35 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(password, salt, (err,hash) => {
+		console.log(hash);
+	});
+});
+
+var hashedPassword = '$2a$10$ju//IhodhYWP3iL5R6pTlOOikoUO8t.6pxC5/Rwhx7rxO6qukZv52';
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+	console.log(result);
+}); 
 
 
-var data = {
-	id:10
-};
 
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded:',decoded);
+// var data = {
+// 	id:10
+// };
+
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded:',decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
