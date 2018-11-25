a25a8ee548dced971c7093b79417b393a448d5e4
Rom Ezer <rom@dotbcs.com>
2018-11-25 13:34:39 +0200

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -22,6 +22,24 @@ app.post('/todos', (req,res) => {
 	})
 });
 
+app.post('/users', (req,res) => {
+	var user = new User({
+		email: req.body.email,
+		password: req.body.password
+	});
+
+	user.save().then(() => {
+		return user.generateAuthToken();
+}).then((token) =>{
+	res.header('x-auth',token).send(user);
+}).catch((e) => {
+		console.log('Error',e);
+		res.status(400).send(e);
+	})
+});
+
+
+
 
 
 
