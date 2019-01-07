07f3a13ff5c0aabee998a5b86f678cd40eed25c9
Rom Ezer <rom@dotbcs.com>
2019-01-07 23:35:03 +0200

    Add Get todos/:id

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/users');
 var {authenticate} = require('./middleware/authenticate');
+const {ObjectID} = require('mongodb');
 
 
 var app = express();
@@ -29,6 +30,22 @@ app.get('/todos',(req,res) =>{
 		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);	
+	});
+});
+
+app.get('/todos/:id',(req,res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) =>{
+		res.status(400).send();
 	});
 });
 
